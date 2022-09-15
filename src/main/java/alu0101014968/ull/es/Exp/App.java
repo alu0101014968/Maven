10a98005d1d86b85fc51282f21c34a25bdfa1140
alu0101014968 <alu0101014968@ull.edu.es>
2022-09-15 01:25:01 +0100

    first

---
+++ src/main/java/alu0101014968/ull/es/Exp/App.java
@@ -0,0 +1,13 @@
+package alu0101014968.ull.es.Exp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/alu0101014968/ull/es/Exp/AppTest.java
@@ -0,0 +1,20 @@
+package alu0101014968.ull.es.Exp;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
